ef047ca4f47053355a12002d97e11f56db3af6cf
gzgywh <1763874803@qq.com>
2019-04-26 00:54:36 +0800

    generate_data

--- README.md
+++ README.md
@@ -3,3 +3,20 @@
 #### 题目描述
 
 100GB url 文件，使用 1GB 内存计算出出现次数 top100 的 url 和出现的次数。
+
+
+
+#### 生成数据
+
+用python脚本生成数据，在这里为了方便我们讲URL的前缀固定为我的github名称 https://github.com/gzgywh/project ，后面加上随机数以区分不同的URL，运行命令如下：
+
+```cc
+python size filename
+```
+
+有2个可配置的参数。size为我要生成多大的数据，单位是GB(在程序内部做了换算)。filename为我要生成程序的名称，比如我要生成一个大小为1GB，名字为test的URL数据，命令如下：
+
+```cc
+python 1 test
+```
+
---
+++ generate.py
@@ -0,0 +1,25 @@
+import sys
+import random
+
+# 乘法快速幂
+def pow(a,b):
+    res=1
+    while b:
+        if (b&1):
+            res*=a
+        a*=a
+        b>>=1
+    return res
+        
+def generate_file(file_size,filename):
+    count =int(file_size)*pow(1024,3)
+    f = open(filename, 'w')
+    while count>0:
+        x = random.randint(10000, 99999)
+        s = 'https://github.com/gzgywh/project/{}\n'.format(x)
+        print(s)
+        count -= len(s)
+        f.write(s)
+
+if __name__=='__main__':
+    generate_file(sys.argv[1],sys.argv[2])
