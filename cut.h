355157bca497da8d337670953106c5c6db323328
gzgywh <1763874803@qq.com>
2019-04-29 10:11:47 +0800

    update

--- cut.h
+++ cut.h
@@ -8,14 +8,14 @@
 using namespace std;
 typedef long long LL;
 
-LL filesize(string filename);
+LL Filesize(string file_name); 
 
-void splitfile(string filename, int &num, int size);
+void Split(string filename, int &subfile, int size);
 
 void merge(string pre,int n,int topK);
 
-void sortFiles(string source_file_name,int topK=INT_MAX);
+void sortFiles(string source,int topK=INT_MAX);
 
-void solve(int file_num,int file_size,int topK);
+void solve(int filenum,int size,int topK);
 
 #endif 
